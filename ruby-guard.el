52b6b150dc0e127a18f1fd4e9d4de9e385cd6be3
Zhang Kai Yu <yeannylam@gmail.com>
2014-11-28 16:03:49 +0800

    Implemented ruby-guard-root

--- ruby-guard.el
+++ ruby-guard.el
@@ -1,9 +1,16 @@
 (defvar ruby-guard-buffer-name "*guard*")
 
-
-
-(defun ruby-guard-available-p ()
-  (file-exists-p (projectile-expand-root "Guardfile")))
+(defun ruby-guard-root (&optional last-directory)
+  "Return the directory name where guard file is located."
+  (ignore-errors
+    (let ((current-directory
+           (or last-directory (file-truename default-directory))))
+      (if (string-equal "/" current-directory)
+          nil
+        (if (file-exists-p (expand-file-name "Guardfile" current-directory))
+            current-directory
+          (setq current-directory (expand-file-name ".." current-directory))
+          (ruby-guard-root current-directory))))))
 
 (defun ruby-guard ()
   (interactive)
