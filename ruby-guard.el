6f51d55e978c9b0756e7f0a8cee8a4d517786912
Zhang Kai Yu <yeannylam@gmail.com>
2014-11-28 16:52:57 +0800

    Implementation.

--- ruby-guard.el
+++ ruby-guard.el
@@ -12,18 +12,40 @@
           (setq current-directory (expand-file-name ".." current-directory))
           (ruby-guard-root current-directory))))))
 
+(defun ruby-guard-spring-p ()
+  (file-exists-p (file-truename
+                  (concat temporary-file-directory
+                          "spring/"
+                          (md5 (ruby-guard-root) 0 -1)
+                          ".pid"))))
+
+(defun ruby-guard-zeus-p ()
+  (file-exists-p (expand-file-name ".zeus.sock" (ruby-guard-root))))
+
+(defun ruby-guard-bundle-p ()
+  (file-exists-p (expand-file-name "Gemfile" (ruby-guard-root))))
+
+(defun ruby-guard-command-name ()
+  (cond ((ruby-guard-spring-p)
+         "spring guard")
+        ((ruby-guard-bundle-p)
+         "bundle exec guard")
+        (t "guard")))
+
+(defmacro ruby-guard-with-root (body-form)
+  `(let ((default-directory (ruby-guard-root)))
+     ,body-form))
+
 (defun ruby-guard ()
   (interactive)
-  (if (ruby-guard-available-p)
-      (progn
-        (if (member projectile-rails-guard-buffer-name
-                    (mapcar 'buffer-name (buffer-list)))
-            (switch-to-buffer projectile-rails-guard-buffer-name)
-          (projectile-rails-with-root
-           (async-shell-command (projectile-rails-with-preloader
-                                 :spring "spring guard"
-                                 :zeus "zeus guard"
-                                 :vanilla "bundle exec guard")
-                                (get-buffer-create
-                                 projectile-rails-guard-buffer-name)))))
-    (message "Guardfile not found.")))
+  (let ((default-directory (ruby-guard-root)))
+    (if default-directory
+        (progn
+          (if (member ruby-guard-buffer-name
+                      (mapcar 'buffer-name (buffer-list)))
+              (switch-to-buffer ruby-guard-buffer-name)
+            (ruby-guard-with-root
+             (async-shell-command
+              (ruby-guard-command-name)
+              (get-buffer-create ruby-guard-buffer-name)))))
+      (message "Cannot find Guardfile."))))
